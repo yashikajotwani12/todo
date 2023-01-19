e0ffd9cba7a2dc0fc434962d27365ee165565175
yashikajotwani12 <yashikajotwani12@gmail.com>
2023-01-19 12:48:58 +0530

    final setup

--- todo/settings.py
+++ todo/settings.py
@@ -114,8 +114,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
