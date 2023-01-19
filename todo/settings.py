873e09f0e7ad949d2194615ba5924681bd9f3c7e
yashikajotwani12 <yashikajotwani12@gmail.com>
2023-01-19 22:39:10 +0530

    final setup

--- todo/settings.py
+++ todo/settings.py
@@ -123,3 +123,7 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    'localhost:3000',
+    'https://todo-production-c20f.up.railway.app/'
+]
