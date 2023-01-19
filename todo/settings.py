3e64aa15216d80ac62768fdb4c5a3a40657bf9b5
yashikajotwani12 <yashikajotwani12@gmail.com>
2023-01-19 23:07:10 +0530

    final setup

--- todo/settings.py
+++ todo/settings.py
@@ -133,4 +133,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = [
     'https://todo-production-c20f.up.railway.app/'
+    'https://todo-production-c20f.up.railway.app/add/'
 ]
